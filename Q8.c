89a64e968433e2d25dccb85335009e8ed1395971
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-09-14 22:10:24 +0530

    Create Q8.c

---
+++ Q8.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+   int a[10],n;
+    int i;
+    printf("Enter the values ");
+    for(i=0;i<=9;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<=9;i++)
+    {
+        if(a[0]>a[i])
+        {
+            
+            n=a[i];
+            a[i]=a[0];
+            a[0]=n;    
+        }
+    }
+    for(i=2;i<=9;i++)
+    {
+        if(a[1]>a[i])
+        {
+            a[1]=a[i];
+        }
+    }
+    printf("The second smallest number is %d",a[1]);
+}
