5bed86fb3ccdcf0fd48aaaebd80f466f7ae73b44
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-09-14 22:09:19 +0530

    Create Q5.c

---
+++ Q5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+   int a[10];
+    int i;
+    printf("Enter the values ");
+    for(i=0;i<=9;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<=9;i++)
+    {
+        if(a[0]>a[i])
+        {
+            a[0]=a[i];    
+        }
+    }
+    printf("The largest number is %d",a[0]);
+}
